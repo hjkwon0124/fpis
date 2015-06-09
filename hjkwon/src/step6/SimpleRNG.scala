781872a3259ddce761860a6796ad43efa1d5d6d6
hjkwon <hjkwon0124@gmail.com>
2015-06-09 14:56:07 +0900

    thanks

--- hjkwon/src/step6/SimpleRNG.scala
+++ hjkwon/src/step6/SimpleRNG.scala
@@ -24,6 +24,33 @@ case class SimpleRNG(seed: Long) extends RNG{
     (if (i < 0) -(i + 1) else i, r)
   }
 
+  def ints(count: Int)(rng : RNG) : (List[Int],RNG)={
+    if(count >0){
+    val (x,r) = rng.nextInt
+    val (xs,r1) = ints(count-1)(r)
+    (x::xs,r1)
+    }
+    else
+      (Nil,rng)
+  }
+
+  def map2[A,B,C](ra:Rand[A], rb:Rand[B])(f: (A,B) => C) : Rand[C]=
+    rng => {
+      val (a, rng1) = ra(rng)
+      val (b, rng2) = rb(rng1)
+      (f(a,b), rng2)
+    }
+
+  def unit[A](a:A): Rand[A]=
+  rng => (a,rng)
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight( unit( List[A]() ) ) ((f, acc) => map2(f, acc)(_ :: _))
+
+  def _ints(count: Int): Rand[List[Int]] =
+    sequence(List.fill(count)(rng => rng.nextInt))
+
+
   def map[A,B](s:Rand[A])(f: A=>B): Rand[B] =
   rng =>{
     val (a,rng2) = s(rng)
--- hjkwon/src/step6/step6_main.scala
+++ hjkwon/src/step6/step6_main.scala
@@ -4,12 +4,17 @@ package step6
  * Created by hjkwon on 15. 6. 4.
  */
 object step6_main extends App{
-  val b=  SimpleRNG(2)
+  val b=  new SimpleRNG(2)
+  val c= new SimpleRNG(3)
 //  val a = SimpleRNG(40).nonNegativeInt(b)
-  val c = SimpleRNG(20).nonNegativeEven
-  val d = SimpleRNG(20).nonNegativeEven(b)
+//  val c = SimpleRNG(20).nonNegativeEven
+//  val d = SimpleRNG(20).nonNegativeEven(b)
 
+  println(b._ints(5)(c))
 
-  println(c)
-  println(d)
+  println(List(1)::List(4))
+//
+//  println(b.ints(5)(c))
+//  println(c)
+
 }
